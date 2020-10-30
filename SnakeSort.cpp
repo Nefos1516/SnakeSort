74d84efbe136d79c704a07b1d209bfde6258d2a7
Nefos1516 <71015653+Nefos1516@users.noreply.github.com>
2020-10-30 23:55:12 +0300

    Added comms

--- SnakeSort.cpp
+++ SnakeSort.cpp
@@ -4,37 +4,40 @@
 #include <locale.h>
 
 main() {
-	setlocale (LC_ALL, "Rus");
+	setlocale (LC_ALL, "Rus");//setting russian
 	
-	srand(time(NULL));
+	srand(time(NULL));//random for autocomplete array
 	
-	int **a;
+	int **a;//pointer to pointer to line of elements in two-dimensional array
 	int m, n;
 	
-	printf("Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚СЂРѕРє РјР°СЃСЃРёРІР°:");
-	scanf("%d", &n);
+	printf("Введите количество строк массива:");
+	scanf("%d", &n);//insput number of lines here
 	
 	printf("\n");
 	
-	printf("Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃС‚РѕР»Р±С†РѕРІ РјР°СЃСЃРёРІР°:");
-	scanf("%d", &m);
+	printf("Введите количество столбцов массива:");
+	scanf("%d", &m);//input number of columns here
 	
-	a = (int**)malloc(n*sizeof(int*));
+	a = (int**)malloc(n*sizeof(int*));//assignment of memory for pointers to lines
 	
 	for (int i = 0; i < n ; i++) {
-		a[i] = (int*)malloc(m*sizeof(int));
+		a[i] = (int*)malloc(m*sizeof(int));//assingment of memory for lines themself
 		for (int j = 0; j < m; j++) {
-			a[i][j] = rand() % 100 - 50;
+			a[i][j] = rand() % 100 - 50;//autocompleting array with random numbers from -50 to 50
 		}
 	}
 	
-	printf("РР·РЅР°С‡Р°Р»СЊРЅС‹Р№ РјР°СЃСЃРёРІ:\n");
+	//output of unsorted array
+	printf("Изначальный массив:\n");
 	for (int i = 0; i < n ; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%4d", a[i][j]);
 		}
 		printf("\n");
 	}
+	
+	//creating one-dimensional array and filling it with elements of array(a) for easier sorting
 	int *h;
 	h = (int*)malloc(n*m*sizeof(int));
 	
@@ -46,6 +49,7 @@ main() {
 		}
 	}
 	
+	//bubble-sort
 	for (int i = 0; i < k; i++) {
 		for (int j = 0; j < k-1; j++){
 			if (h[j]>h[j+1]) {
@@ -56,40 +60,49 @@ main() {
 		}
 	}
 	
+	/*this is borders of array(a), which will help us to put numbers in the right way
+	t - top, r - right, b - bottom, l - left*/
 	int t = 0, r = m - 1, b = n - 1, l = 0;
 	
-	int count = 0;
+	int count = 0;//just counter of iterations
+	//starting while cycle until our elements will end, or top and bottom or right and left borders will meet
 	while (t < b || l < r || count < k) {
+		
+		//putting sorted elements from array(h) to array(a) from left border to right border
 		for (int i = l; i <= r; i++) {
 			a[t][i] = h[count];
 			count++;
 		}
-		t++;
-		if (t > b) break;
+		t++;//our top border is moving down by 1
+		if (t > b) break;//breaking out of cycle while if top border is lower than bottom border
 		
+		//putting sorted elements from array(h) to array(a) from top border to bottom border
 		for (int i = t; i <= b; i++) {
 			a[i][r] = h[count];
 			count++;
 		}
-		r--;
-		if (l > r) break;
+		r--;//our right border is moving left by 1
+		if (l > r) break;//breaking out of cycle while if right border is more left than left border
 		
+		//putting sorted elements from array(h) to array(a) from right border to left border
 		for (int i = r; i >= l; i--) {
 			a[b][i] = h[count];
 			count++;
 		}
-		b--;
+		b--;//our bottom border is moving up by 1
 		if(t > b) break;
 		
+		//putting sorted elements from array(h) to array(a) from bottom border to top border
 		for (int i = b; i >= t; i--) {
 			a[i][l] = h[count];
 			count++;
 		}
-		l++;
+		l++;//our left border is moving right by 1
 		if(l > r) break;
 	}
 	
-	printf("РћС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ РјР°СЃСЃРёРІ:\n");
+	//output of sorted array
+	printf("Отсортированный массив:\n");
 	for (int i = 0; i < n ; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%4d", a[i][j]);
